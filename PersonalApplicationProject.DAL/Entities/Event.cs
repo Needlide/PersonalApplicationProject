3f65bd9ec98bae9e28c42d3a9a64e6536872a942
Needlide <max.litvinyuck553@gmail.com>
2025-10-22 00:04:43 +0300

    refactor(DAL): initialize properties with `string.Empty`

--- PersonalApplicationProject.DAL/Entities/Event.cs
+++ PersonalApplicationProject.DAL/Entities/Event.cs
@@ -7,7 +7,7 @@ public class Event
 {
     [Key] public int Id { get; set; }
 
-    [Required] [MaxLength(255)] public string Name { get; set; }
+    [Required] [MaxLength(255)] public string Name { get; set; } = string.Empty;
 
     [MaxLength(255)] public string? Description { get; set; }
 
--- PersonalApplicationProject.DAL/Entities/User.cs
+++ PersonalApplicationProject.DAL/Entities/User.cs
@@ -7,13 +7,13 @@ public class User
 {
     [Key] public int Id { get; set; }
 
-    [Required] [MaxLength(255)] public string Email { get; set; }
+    [Required] [MaxLength(255)] public string Email { get; set; } =  string.Empty;
 
-    [Required] public string PasswordHash { get; set; }
+    [Required] public string PasswordHash { get; set; } =  string.Empty;
 
-    [Required] [MaxLength(100)] public string FirstName { get; set; }
+    [Required] [MaxLength(100)] public string FirstName { get; set; } = string.Empty;
 
-    [Required] [MaxLength(100)] public string LastName { get; set; }
+    [Required] [MaxLength(100)] public string LastName { get; set; } = string.Empty;
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
