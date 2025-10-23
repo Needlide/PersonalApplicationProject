afb9a91eb4f5be53020cf07e7cab525b9045c467
Needlide <max.litvinyuck553@gmail.com>
2025-10-23 20:10:40 +0300

    feat(API): add code to automatically migrate on startup

--- PersonalApplicationProject.API/Program.cs
+++ PersonalApplicationProject.API/Program.cs
@@ -157,6 +157,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
+
 app.UseExceptionHandler(_ => { });
 app.UseHttpsRedirection();
 app.UseStatusCodePages();
