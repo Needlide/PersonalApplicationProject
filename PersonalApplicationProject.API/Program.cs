7d63fc249b9f81935eb71ecd3acf2aece776067f
Needlide <max.litvinyuck553@gmail.com>
2025-10-23 20:05:46 +0300

    feat(API): remove migration code (handled by service from docker compose)

--- PersonalApplicationProject.API/Program.cs
+++ PersonalApplicationProject.API/Program.cs
@@ -157,12 +157,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    await dbContext.Database.MigrateAsync();
-}
-
 app.UseExceptionHandler(_ => { });
 app.UseHttpsRedirection();
 app.UseStatusCodePages();
